3364802f7a350da6a3c8a6d2fbd1837e5f86f5d4
Marcio Gadelha <zagadelha@gmail.com>
2021-06-15 14:53:15 -0300

    Adjusts

---
+++ contact-book-config/src/main/java/com/contactbook/ContactBookConfigApplication.java
@@ -0,0 +1,15 @@
+package com.contactbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ContactBookConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBookConfigApplication.class, args);
+	}
+
+}
